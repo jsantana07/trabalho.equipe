f14862524e28a9143fc05e1c5461ec77dd625274
julia 26/07 <jsantana23julia@gmail.com>
2024-07-26 11:02:04 -0300

    julinha inicio das telas 26/07

--- trabalhojulia07/obj/Debug/net8.0-ios/iossimulator-x64/trabalhojulia07.AssemblyInfo.cs
+++ trabalhojulia07/obj/Debug/net8.0-ios/iossimulator-x64/trabalhojulia07.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("trabalhojulia07")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b3776f3c1dcfc6207268c3f80cc0b0f364c09816")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+847886f0d743815bf3514687f9b94023013c564a")]
 [assembly: System.Reflection.AssemblyProductAttribute("trabalhojulia07")]
 [assembly: System.Reflection.AssemblyTitleAttribute("trabalhojulia07")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
